f5a2d95ce099f483daad9fe61025d9cf22787c4f
Ángel <angelmonclova@gmail.com>
2023-01-28 18:19:57 +0100

    Create github/workflow folder and add inside yml

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-export function suma(a, b) {
-  return a + b;
+// export function suma(a, b) {
+//   return a + b;
+// }
+
+// const containerArray = [];
+
+let myArray = [
+  ["Antonio", "Antonio", "Paco"],
+  ["Nina", "Andrea", "Antonio", "Eli"],
+  ["Lana", "Antonio", "Plata"],
+];
+
+function runArr() {
+  let nameCount = 0;
+  for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+    for (let j = 0; j < myArray.length; j++) {
+      if (myArray[i][j] === "Antonio") {
+        nameCount++;
+        console.log(myArray[i][j]);
+        console.log(nameCount);
+      }
+    }
+  }
 }
+
+runArr();
